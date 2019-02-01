ce58e88d43e4145f459c0ccc35440415e9ec607c
gabrielloye <gabrielloye@hotmail.com>
2019-02-01 17:22:40 +0800

    uploaded onto github pages

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   @Input()positions: object;
-  images = ['/assets/images/united.jpg','/assets/images/city.jpg','/assets/images/pool.jpg','/assets/images/chelsea.jpg','/assets/images/ars.jpg','/assets/images/spurs.jpg']
+  images = ['assets/images/united.jpg','assets/images/city.jpg','assets/images/pool.jpg','assets/images/chelsea.jpg','assets/images/ars.jpg','assets/images/spurs.jpg']
   
   constructor() { }
 
