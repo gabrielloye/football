1e2a8e5199eb4bb2323d43ccfcd01e78a6b181e4
Gabriel Loye (MM-SG) <gabriel_loye@mentormedia.com>
2019-02-01 09:47:06 +0800

    Auto-generated commit

---
+++ out-tsc/src/app/app.module.js
@@ -0,0 +1,29 @@
+import * as tslib_1 from "tslib";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib_1.__decorate([
+        NgModule({
+            declarations: [
+                AppComponent,
+                CarouselComponent
+            ],
+            imports: [
+                BrowserModule,
+                AppRoutingModule,
+                NgbCarouselModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
