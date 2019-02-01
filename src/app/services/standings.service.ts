9d8a08c1f827aa112fd5eed6286efba5b4b5dcef
Gabriel Loye (MM-SG) <gabriel_loye@mentormedia.com>
2019-02-01 10:11:33 +0800

    changed http to https

--- src/app/services/standings.service.ts
+++ src/app/services/standings.service.ts
@@ -22,27 +22,27 @@ export class StandingsService {
   getStandings(): Observable<any> {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','e19fdf16497e43a4ad3141d5daf3caff')
-    return this.http.get('http://api.football-data.org/v2/competitions/PL/standings', httpOptions)
+    return this.http.get('https://api.football-data.org/v2/competitions/PL/standings', httpOptions)
     .pipe(catchError(this.httperror.handleError))
   };
 
   getTeams(): Observable<any> {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','e19fdf16497e43a4ad3141d5daf3caff')
-    return this.http.get('http://api.football-data.org/v2/competitions/PL/teams', httpOptions)
+    return this.http.get('https://api.football-data.org/v2/competitions/PL/teams', httpOptions)
     .pipe(catchError(this.httperror.handleError))
   }
   getTeam (id: string): Observable<any> {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','e19fdf16497e43a4ad3141d5daf3caff')
-    const link = 'http://api.football-data.org/v2/teams/' + id;
+    const link = 'https://api.football-data.org/v2/teams/' + id;
     return this.http.get(link, httpOptions)
     .pipe(catchError(this.httperror.handleError))
   }
   getTopScorers(limit: number): Observable<any> {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','e19fdf16497e43a4ad3141d5daf3caff')
-    const link = 'http://api.football-data.org/v2/competitions/PL/scorers?limit=' + limit;
+    const link = 'https://api.football-data.org/v2/competitions/PL/scorers?limit=' + limit;
     return this.http.get(link, httpOptions)
     .pipe(catchError(this.httperror.handleError))
   }
@@ -50,7 +50,7 @@ export class StandingsService {
   getSchedule(matchday: number) {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','d256c8829c5f44d195b07dcb46e4fc91')
-    const link = 'http://api.football-data.org/v2/competitions/PL/matches?status=SCHEDULED&matchday=' + matchday;
+    const link = 'https://api.football-data.org/v2/competitions/PL/matches?status=SCHEDULED&matchday=' + matchday;
     return this.http.get(link, httpOptions)
     .pipe(catchError(this.httperror.handleError))
   }
@@ -58,7 +58,7 @@ export class StandingsService {
   getResult(matchday: number) {
     httpOptions.headers = 
     httpOptions.headers.set('X-Auth-Token','d256c8829c5f44d195b07dcb46e4fc91')
-    const link = 'http://api.football-data.org/v2/competitions/PL/matches?status=FINISHED&matchday=' + matchday;
+    const link = 'https://api.football-data.org/v2/competitions/PL/matches?status=FINISHED&matchday=' + matchday;
     return this.http.get(link, httpOptions)
     .pipe(catchError(this.httperror.handleError))
   }
