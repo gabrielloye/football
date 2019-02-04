0ef18c5d138a0d899bdb91a420d83e331193432e
gabrielloye <gabrielloye@hotmail.com>
2019-02-04 12:00:49 +0800

    minor updates to results, login and homepage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLJgBwRNdOTNtTLQFdJ4rAOc16PSYYvzU",
+    authDomain: "login-2576f.firebaseapp.com",
+    databaseURL: "https://login-2576f.firebaseio.com",
+    projectId: "login-2576f",
+    storageBucket: "login-2576f.appspot.com",
+    messagingSenderId: "352199938211"
+  }
 };
